818cb2fc6aa3524b234a6a122336d81addf7b219
Dedeti <andrenes2905@gmail.com>
2023-11-10 14:56:23 -0300

    adicionar um texto na VerdadeView

--- Sources/DedetiKit/DedetiKit.swift
+++ Sources/DedetiKit/DedetiKit.swift
@@ -1,2 +1,13 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct VerdadeView: View {
+    public init(){}
+    public var body: some View {
+        Text("Bonito é feio!")
+    }
+}
+
+
+#Preview {
+    VerdadeView()
+}
